dd776e3f2fc4b7517f467c775158dec543b5e200
Bryant <bryant.franks@gmail.com>
2021-02-13 12:19:12 -0500

    add length validation to input and fixed punctuation

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -14,7 +14,7 @@ moveCommands = {'north', 'east', 'south', 'west', 'exit'}
 # Current Player Status function
 def player_status(currentRoom):
     print('_______________________________________')
-    print('You are in the {}'.format(currentRoom))
+    print('You are in the {}.'.format(currentRoom))
 
 
 # Player action function. TODO: verb is present for future iteration of game
@@ -25,11 +25,11 @@ def player_action(presentRoom, verb, noun):
         # verifies movement action is valid
         if noun in rooms[presentRoom]:
             currentRoom = rooms[presentRoom][noun]
-            print('You decide to follow the path to the {}'.format(noun))
+            print('You decide to follow the path to the {}.'.format(noun))
         else:
-            print('There is not path to the {}.'.format(noun))
+            print('There is no path to the {}.'.format(noun))
     else:
-        print('This is not permitted')
+        print('This is not permitted.')
 
 
 # Begin Game
@@ -38,21 +38,25 @@ def player_action(presentRoom, verb, noun):
 currentRoom = 'Great Hall'
 
 # show game instructions
-print('This is the simplified dragon game')
-print('Type the directions: north, east, south, west to move, or exit to quit')
+print('This is the simplified dragon game.')
+print('Type the instruction: Go North, Go South, Go East, or Go West. Type Exit to quit.')
 
 # begin game loop
 while True:
     # Game Logic that runs prior to each player movement
     player_status(currentRoom)
-    userInput = input('Enter a move: ')
+    userInput = input('Enter a move: Go North, Go South, Go East, or Go West. Type Exit to quit.')
     inputMove = userInput.lower().split()
 
     # validate input action and leaves on exit
     if 'exit' not in inputMove:
-        directive = inputMove[0]
-        actionSubject = inputMove[1]
-        player_action(currentRoom, directive, actionSubject)
+        # Validates input move length before continuing.
+        if len(inputMove) != 2:
+            print('Something went wrong with your input, try again.')
+        else:
+            directive = inputMove[0]
+            actionSubject = inputMove[1]
+            player_action(currentRoom, directive, actionSubject)
     else:
         print('Thanks for Playing!')
         break
